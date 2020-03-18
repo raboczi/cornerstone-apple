cbadecb6ed30b3fd461ee44badbb2741cc57fece
raboczi <raboczi@icloud.com>
2020-03-18 21:31:51 +1000

    Initial Commit

---
+++ CornerstoneTests/CornerstoneTests.swift
@@ -0,0 +1,34 @@
+//
+//  CornerstoneTests.swift
+//  CornerstoneTests
+//
+//  Created by Simon Raboczi on 18/3/20.
+//  Copyright © 2020 Simon Raboczi. All rights reserved.
+//
+
+import XCTest
+@testable import Cornerstone
+
+class CornerstoneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
