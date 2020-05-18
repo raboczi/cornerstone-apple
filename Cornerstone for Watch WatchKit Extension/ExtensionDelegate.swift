ed10df88d004c0729457f1b060a41717af9da82f
raboczi <raboczi@icloud.com>
2020-05-18 14:29:42 +1000

    Corrected CornerstoneClient to build with iOS. Added OSGi event to websocket bridge start/stop to the watch app.

--- Cornerstone for Watch WatchKit Extension/ExtensionDelegate.swift
+++ Cornerstone for Watch WatchKit Extension/ExtensionDelegate.swift
@@ -6,21 +6,26 @@
 //  Copyright © 2020 Simon Raboczi. All rights reserved.
 //
 
+import CornerstoneClient
 import WatchKit
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
+    let bridge = Bridge()
+    
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
     }
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        bridge.start()
     }
 
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
+        bridge.stop()
     }
 
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
