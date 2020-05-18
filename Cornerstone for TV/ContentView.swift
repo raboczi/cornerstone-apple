fc236baf9b42929af2693f5a0903e95e6bc88b09
raboczi <raboczi@icloud.com>
2020-05-18 17:23:31 +1000

    Added Cornerstone for TV

---
+++ Cornerstone for TV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cornerstone for TV
+//
+//  Created by Simon Raboczi on 18/5/20.
+//  Copyright © 2020 Simon Raboczi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
